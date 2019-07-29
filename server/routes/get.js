28eda22abd8040e0b81d25b607b7c4640ba6035d
PRO-BIT\janezs <dsada@gmail.com>
2019-07-29 14:53:26 +0200

    backend routess

--- server/routes/get.js
+++ server/routes/get.js
@@ -1,46 +1,146 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require("mongoose");
-
-const mongoClient = require('./mongodbConnModule');
-var db = mongoClient._connect();
-
-const {
-    Student, Teacher, School, SClass,
-    Year, Competition, Achivment, AchivmentLevel,
-    AchivmentStudent, CompetitionStudent, ClassSubject,
-    Subject, AchivmentType, Grade, ClassStudentYear,
-    CompetitionSubject, ClassCompetition
-} = require('./../models/schemas');
-
 
 const QueryBuilder = require('node-querybuilder');
 const settings = {
     host: 'localhost',
     port: '3306',
-    database: 'razvrscanje_dijakov',
+    database: 'SZR_DB',
     user: 'root',
-    password: ''
+    password: 'root'
 };
 
+class DBMethods {
+    static getClasses() {
+        return new Promise(resolve => {
+            const qb = new QueryBuilder(settings, 'mysql', 'single');
 
-router.get('/mysql-test', (req, res, next) => {
-    const qb = new QueryBuilder(settings, 'mysql', 'single');
- 
-    qb.select('ime, tocke').get('nivoji', (err, result) => {
-        qb.disconnect();
-        res.status(200).json({
-            ok: true,
-            result: result 
+            qb.select("c.id, c.name as class, s.name").from('classes c')
+                .join('schools s', 's.id=c.school_id', 'left')
+                .get((err, result) => {
+                    qb.disconnect();
+                    resolve(result);
+                });
         });
+    }
+
+    static getSubjects() {
+        return new Promise(resolve => {
+            const qb = new QueryBuilder(settings, 'mysql', 'single');
+
+            qb.select("cs.class_id, s.name, s.short_name").from('classes_subjects cs')
+                .join('subjects s', 's.id=cs.subject_id', 'left')
+                .get((err, result) => {
+                    qb.disconnect();
+                    resolve(result);
+                });
+        });
+    }
+
+    static getFormatedClasses() {
+        return new Promise(async resolve => {
+            let classRes = await this.getClasses();
+            let subjectsRes = await this.getSubjects();
+    
+            resolve(classRes.map(citem => {
+                citem['subjects'] = subjectsRes
+                    .filter(x => x.class_id == citem.id)
+                    .map(x => {
+                        delete x.class_id
+                        return x;
+                    });
+        
+                delete citem.id;
+                return citem;
+            })); 
+        });
+    }
+}
+
+
+router.get('/classes', async (req, res, next) => {
+    res.status(200).json({
+        ok: true,
+        result: await DBMethods.getFormatedClasses()
     });
 });
 
 router.get('/students', (req, res, next) => {
-    Student.find({}, 'name surname easistentId', function (error, students) {
-        if (error) console.error(error);
-        res.status(200).json({ students });
-    }).sort({ _id: -1 });
+    const qb = new QueryBuilder(settings, 'mysql', 'single');
+
+    qb.select("*").from('students')
+        .get((err, result) => {
+            qb.disconnect();
+            res.status(200).json({
+                ok: true,
+                result: result
+            });
+        });
+});
+
+router.get('/students-by-year', (req, res, next) => {
+    if (Object.values(req.body).includes('')) {
+        res.status(200).json({
+            ok: false,
+            result: 'Obrazec ni bil pravilno izpolnjen!'
+        });
+    } else {
+        const qb = new QueryBuilder(settings, 'mysql', 'single');
+
+        qb.select([
+            's.name as studentname',
+            's.surname',
+            'c.name as class',
+            's.mail',
+            'y.name as year',
+            'ss.name as school',
+            'gs.confirmed_grades'
+        ]).from('gstudents gs')
+            .where('year_id', req.body.id)
+            .join('students s', 's.id=gs.student_id', 'left')
+            .join('classes c', 'c.id=gs.class_id', 'left')
+            .join('years y', 'y.id=gs.year_id', 'left')
+            .join('schools ss', 'ss.id=c.school_id', 'left')
+            .get((err, result) => {
+                qb.disconnect();
+                res.status(200).json({
+                    ok: true,
+                    result: result
+                });
+            });
+    }
+});
+
+
+router.get('/achivments', (req, res, next) => {
+    if (Object.values(req.body).includes('')) {
+        res.status(200).json({
+            ok: false,
+            result: 'Obrazec ni bil pravilno izpolnjen!'
+        });
+    } else {
+        const qb = new QueryBuilder(settings, 'mysql', 'single');
+
+        qb.select([
+            's.name as studentname',
+            's.surname',
+            'a.name as achivments',
+            'a.description',
+            't.name as type',
+            'l.name as level'
+        ]).from('achivments a')
+            .join('achivments_students  as', 'as.achivment_id = a.id', 'left')
+            .join('types t', 't.id = a.type_id', 'left')
+            .join('levels l', 'l.id = a.level_id', 'left')
+            .join('students s', 's.id = as.student_id', 'left')
+            .get((err, result) => {
+                qb.disconnect();
+                res.status(200).json({
+                    ok: true,
+                    result: result
+                });
+            });
+    }
 });
 
 router.post('/get-student-by-id', (req, res, next) => {
@@ -50,57 +150,71 @@ router.post('/get-student-by-id', (req, res, next) => {
             result: 'Obrazec ni bil pravilno izpolnjen!'
         });
     } else {
-        Student.findOne({
-            _id: mongoose.Types.ObjectId(req.body.id)
-        }, async function (error, result) {
-            if (error) {
+        const qb = new QueryBuilder(settings, 'mysql', 'single');
+
+        qb.select("*").from('students')
+            .where('id', req.body.id)
+            .get((err, result) => {
+                qb.disconnect();
                 res.status(200).json({
-                    ok: false,
-                    result: 'Pojavila se je napaka pri iskanju uporabnika!'
+                    ok: true,
+                    result: result
                 });
-            } else {
-                if (!result) {
-                    res.status(200).json({
-                        ok: false,
-                        result: 'Zapis dijaka ni bil najden!!'
-                    });
-                } else {
-                    res.status(200).json({
-                        ok: true,
-                        result: result
-                    });
-                }
-            }
-        });
+            });
     }
 });
 
 router.get('/teachers', (req, res, next) => {
-    Teacher.find({}, 'name surname email', function (error, teachers) {
-        if (error) console.error(error);
-        res.status(200).json({ teachers });
-    }).sort({ _id: -1 });
+    const qb = new QueryBuilder(settings, 'mysql', 'single');
+
+    qb.select("*").from('teachers')
+        .get((err, result) => {
+            qb.disconnect();
+            res.status(200).json({
+                ok: true,
+                result: result
+            });
+        });
 });
 
 router.get('/teachers-for-picker', (req, res, next) => {
-    Teacher.find({}, 'name surname', function (error, teachers) {
-        if (error) console.error(error);
-        res.status(200).json({ data: teachers });
-    }).sort({ _id: -1 });
+    const qb = new QueryBuilder(settings, 'mysql', 'single');
+
+    qb.select("name, surname").from('teachers')
+        .get((err, result) => {
+            qb.disconnect();
+            res.status(200).json({
+                ok: true,
+                result: result
+            });
+        });
 });
 
+
 router.get('/types', (req, res, next) => {
-    AchivmentType.find({}, 'name points', function (error, types) {
-        if (error) console.error(error);
-        res.status(200).json({ types });
-    }).sort({ _id: -1 });
+    const qb = new QueryBuilder(settings, 'mysql', 'single');
+
+    qb.select("*").from('types')
+        .get((err, result) => {
+            qb.disconnect();
+            res.status(200).json({
+                ok: true,
+                result: result
+            });
+        });
 });
 
 router.get('/levels', (req, res, next) => {
-    AchivmentLevel.find({}, 'name points', function (error, levels) {
-        if (error) console.error(error);
-        res.status(200).json({ levels });
-    }).sort({ _id: -1 });
+    const qb = new QueryBuilder(settings, 'mysql', 'single');
+
+    qb.select("*").from('levels')
+        .get((err, result) => {
+            qb.disconnect();
+            res.status(200).json({
+                ok: true,
+                result: result
+            });
+        });
 });
 
 //this should be moved to another file but for now its here
@@ -122,19 +236,6 @@ router.get('/add-level', (req, res, next) => {
 
 });
 
-router.get('/subjects', (req, res, next) => {
-    res.status(200).json({
-        subjects: [
-            { short: 'mat', long: 'Matematika' },
-            { short: 'slo', long: 'Slovenščina' },
-            { short: 'ang', long: 'Angleščina' },
-            { short: 'mat', long: 'Matematika' },
-            { short: 'slo', long: 'Slovenščina' },
-            { short: 'ang', long: 'Angleščina' }
-        ]
-    });
-});
-
 
 router.get('/competitions', (req, res, next) => {
     res.status(200).json({
@@ -147,40 +248,9 @@ router.get('/competitions', (req, res, next) => {
     });
 });
 
-router.get('/user', (req, res, next) => {
-    if (req.session.id) {
-        res.status(200).json({
-            user: req.session.id
-        });
-    } else {
-        req.session.id = null
-    }
-});
-
-router.get('/classes', (req, res, next) => {
-    res.status(200).json({
-        classes: [
-            { name: '1.tra' },
-            { name: '2.tra' },
-            { name: '3.tra' },
-            { name: '4.tra' },
-            { name: '1.trb' },
-            { name: '2.trb' }
-        ]
-    });
-});
-
 router.get('/competitions', (req, res, next) => {
     res.status(200).json({
         competitions: [{ name: 'Janez', surname: 'Kekec' },
-        { name: 'Luka', surname: 'Neki' },
-        { name: 'Pajdo', surname: 'Pajdo' }]
-    });
-});
-
-router.get('/achivments', (req, res, next) => {
-    res.status(200).json({
-        achivments: [{ name: 'Janez', surname: 'Kekec' },
         { name: 'Luka', surname: 'Neki' },
         { name: 'Pajdo', surname: 'Pajdo' }]
     });
